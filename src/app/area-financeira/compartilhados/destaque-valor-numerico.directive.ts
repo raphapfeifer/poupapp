3c7dd9d20f2060ee3a7eca631d0d184f7f99a4ed
unknown <raphael.alcateia>
2025-12-02 14:59:28 -0300

    Sending value to a directive

--- src/app/area-financeira/compartilhados/destaque-valor-numerico.directive.ts
+++ src/app/area-financeira/compartilhados/destaque-valor-numerico.directive.ts
@@ -1,12 +1,19 @@
-import { afterRender, Directive, ElementRef } from "@angular/core";
+import { afterRender, Directive, ElementRef, input } from "@angular/core";
 
 @Directive({
     selector: '[appDestaqueValorNumerico]'
 })
 export class DestaqueValorNumericoDirective{
+   appDestaqueValorNumerico = input.required<number>();
+
     constructor(elemento: ElementRef<HTMLElement>){
         afterRender(() => {
-            elemento.nativeElement.style.color = 'var(--destaque-receita)';
+            if(this.appDestaqueValorNumerico() > 0){
+                elemento.nativeElement.style.color = 'var(--destaque-receita)';
+            }else if (this.appDestaqueValorNumerico() < 0){
+                elemento.nativeElement.style.color = 'var(--destaque-despesa)';
+            }
+            
         });       
     }
 }
