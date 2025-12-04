a2901f521ace176d71fbf97d440afa30aab5d4dd
unknown <raphael.alcateia>
2025-12-04 14:46:15 -0300

    Refactoring

--- src/app/area-financeira/compartilhados/destaque-valor-numerico.directive.ts
+++ src/app/area-financeira/compartilhados/destaque-valor-numerico.directive.ts
@@ -6,12 +6,15 @@ import { afterRender, Directive, ElementRef, input } from "@angular/core";
 export class DestaqueValorNumericoDirective{
    appDestaqueValorNumerico = input.required<number>();
 
+   corPositiva = input('var(--destaque-receita)');
+   corNegativa = input('var(--destaque-despesa)');
+
     constructor(elemento: ElementRef<HTMLElement>){
         afterRender(() => {
             if(this.appDestaqueValorNumerico() > 0){
-                elemento.nativeElement.style.color = 'var(--destaque-receita)';
+                elemento.nativeElement.style.color = this.corPositiva();
             }else if (this.appDestaqueValorNumerico() < 0){
-                elemento.nativeElement.style.color = 'var(--destaque-despesa)';
+                elemento.nativeElement.style.color = this.corNegativa();
             }
             
         });       
