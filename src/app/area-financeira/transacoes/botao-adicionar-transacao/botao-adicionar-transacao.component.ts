b4667b7b626863f2458452493c4130ceef76b0f5
unknown <raphael.alcateia>
2025-12-15 14:53:02 -0300

    modal refactoring

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,17 +1,18 @@
 import { Component, ElementRef, viewChild } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
+import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [BotaoComponent],
+  imports: [BotaoComponent, ModalComponent],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
 export class BotaoAdicionarTransacaoComponent {
 
-  modal= viewChild.required<ElementRef<HTMLDialogElement>>('modal');
+  
 
   abrirModal(){
-    this.modal().nativeElement.showModal();
+    
   }
 }
---
+++ src/app/compartilhados/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import { afterRender, Component, ElementRef, input, viewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.css'
+})
+export class ModalComponent {
+  modal= viewChild.required<ElementRef<HTMLDialogElement>>('modal');
+
+  aberto = input(false);
+
+  constructor(){
+    afterRender(() => {
+      if(this.aberto()){
+        this.modal().nativeElement.showModal();
+      }    
+    })
+  }
+}
