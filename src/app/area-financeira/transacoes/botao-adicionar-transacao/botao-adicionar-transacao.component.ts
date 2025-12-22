e37e15f15ecf20c22db6223c520cf2eb31c24e36
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2025-12-22 01:21:24 -0300

    Modeling the object

--- src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
+++ src/app/area-financeira/transacoes/botao-adicionar-transacao/botao-adicionar-transacao.component.ts
@@ -1,11 +1,13 @@
-import { afterRender, Component, effect, signal } from '@angular/core';
+import { afterRender, Component, signal } from '@angular/core';
 import { BotaoComponent } from '../../../compartilhados/botao/botao.component';
 import { ModalComponent } from "../../../compartilhados/modal/modal.component";
 import { FormsModule } from "@angular/forms";
+import { TipoTransacao, Transacao } from '../../compartilhados/transacao.model';
+import { KeyValuePipe } from '@angular/common';
 
 @Component({
   selector: 'app-botao-adicionar-transacao',
-  imports: [ModalComponent, BotaoComponent, FormsModule],
+  imports: [ModalComponent, BotaoComponent, FormsModule, KeyValuePipe],
   templateUrl: './botao-adicionar-transacao.component.html',
   styleUrl: './botao-adicionar-transacao.component.css'
 })
@@ -21,6 +23,8 @@ export class BotaoAdicionarTransacaoComponent {
     conta: ''
   }
 
+  tiposTransacao = TipoTransacao;
+
   constructor(){
     afterRender(() => {
       console.log(this.modalAberto());
@@ -33,6 +37,14 @@ export class BotaoAdicionarTransacaoComponent {
   }
 
   aoSubmeter(){
-    console.log(this.novaTransacaoForm);
-  }
+    const novaTransacao = new Transacao(
+      this.novaTransacaoForm.nome,
+      this.novaTransacaoForm.tipo as TipoTransacao,
+      Number(this.novaTransacaoForm.valor),
+      this.novaTransacaoForm.data,
+      this.novaTransacaoForm.conta
+    );
+
+    console.log(novaTransacao);
+}
 }
